09c8db5938b6e1d3cb9406eaca6a808bc216765d
Dennis Snell <dennis.snell@automattic.com>
2015-12-01 04:06:08 -0600

    Only grab necessary data from API

--- src/reducers.js
+++ src/reducers.js
@@ -31,7 +31,7 @@ export const addPost = post => ( {
 } );
 
 export const fetchPosts = dispatch => {
-	fetch( `https://public-api.wordpress.com/rest/v1.1/sites/${ config.site }/posts/` )
+	fetch( `https://public-api.wordpress.com/rest/v1.1/sites/${ config.site }/posts/?fields=ID,attachments,title,content` )
 		.then( response => response.json() )
 		.then( data => data.posts )
 		.then( posts => posts.map( post => ( {
